2080a8e53bb629207d6c780335c7ac448f306527
dayannealcantara <dayannealcantara14@gmail.com>
2024-05-29 22:23:44 -0300

    feat: developing and styling the establishment edit page

---
+++ src/pages/editAddress/index.tsx
@@ -0,0 +1,39 @@
+import { useContext } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import FormAddress, { Address } from '../../components/FormAddress';
+import { AddressContext } from '../../context/AddressContext';
+import { TitleAddAddress, WrapperAddAddress } from '../addAddress/styles';
+
+const EditAddress = () => {
+  const { id } = useParams();
+  const { addresses, updateAddress } = useContext(AddressContext)!;
+  const navigate = useNavigate();
+
+  const idString = id !== undefined ? id : '';
+  const idNumber = Number(idString);
+  const addressToEdit = addresses.find((address) => address.id === idNumber);
+
+  const handleUpdateAddress = (updatedAddress: Address) => {
+    updateAddress(updatedAddress.id, updatedAddress);
+    navigate('/');
+  };
+
+  if (!addressToEdit) {
+    return <TitleAddAddress>Endereço não encontrado!</TitleAddAddress>;
+  }
+
+  return (
+    <WrapperAddAddress>
+      <TitleAddAddress>Editar Endereço</TitleAddAddress>
+      {addressToEdit && (
+        <FormAddress
+          initialAddress={addressToEdit}
+          onSubmit={handleUpdateAddress}
+        />
+      )}
+    </WrapperAddAddress>
+  );
+};
+
+export default EditAddress;
